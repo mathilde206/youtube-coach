4f18e995c84cd853d095809d1c2d4de554731b2b
Mathilde <mathilde206@gmail.com>
2018-04-01 16:06:30 +0000

    Add filtering functionality on get_videos

--- app.py
+++ app.py
@@ -17,28 +17,76 @@ app.config['MONGO_URI'] = config.MONGO_URI
 mongo = PyMongo(app)
 
 
+def get_mongo_search_query(value, search_query):
+    if len(search_query) == 1:
+        return {value: search_query[0]}
+    else:
+        arr = []
+        for item in search_query:
+            arr.append({value:item})
+        return {'$or': arr}
+        
+def append_to_query(value, query_arr):
+    if request.form.get(value):
+            category_name_query = get_mongo_search_query(value, request.form.getlist(value))
+            return query_arr.append(category_name_query)
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/get_videos/<category>/<page_number>")
+@app.route("/get_videos/<category>/<page_number>", methods=["GET", "POST"])
 def get_videos(category, page_number):
+    """
+    This function will show the videos with and without filtering, by creating a query_filter string that will be used to retrieve the right videos.
+    If there is no POST request, the filter is simply the category or empty if no category. 
+    """
+    query_arr = []
+    query_filter=''
+
+    if request.method == "POST":
+        append_to_query('category_name', query_arr)
+        append_to_query('body_part_name', query_arr)
+        append_to_query('language_name', query_arr)
+        
+        if len(query_arr) == 0:
+            query_filter = ""
+        else: 
+            query_filter = {'$and':query_arr}
+    
+    if query_filter == '': 
+        if category != "all":
+            query_filter = {'category_name':category}
+    
+    #Create a dict to identify in the template what was selected 
+    selected = {
+        'category_name': request.form.getlist("category_name") or [],
+        'body_part_name': request.form.getlist("body_part_name") or [],
+        'language_name': request.form.getlist("language_name") or [],
+    }
+    print(selected)
+    
+    #Retrieve the filtered videos (or all) and the right amount of videos for pagination
     skips = 9*(int(page_number)-1)
 
-    if category == "all":
+    if category == "all" and query_filter == "":
         videos=mongo.db.videos.find().skip(skips).limit(9)
         number_of_pages= math.ceil(mongo.db.videos.count()/9)
     else:
-        videos=mongo.db.videos.find({"category_name": category}).skip(skips).limit(9)
-        print(videos[0])
-        number_of_pages= math.ceil(mongo.db.videos.count({"category_name": category})/9)
+        videos=mongo.db.videos.find(query_filter).skip(skips).limit(9)
+        number_of_pages= math.ceil(mongo.db.videos.count(query_filter)/9)
 
     return render_template("get_videos.html",
                             videos=videos,
                             category=category,
+                            categories= mongo.db.categories.find(),
+                            targetted_body_parts=mongo.db.targetted_body_parts.find(),
+                            languages=mongo.db.languages.find(),
                             number_of_pages=number_of_pages,
-                            page_number=page_number)
+                            page_number=page_number,
+                            selected=selected)
 
 
 @app.route("/add_video")
@@ -98,7 +146,6 @@ def delete_video(video_id):
 
 @app.route("/edit_video/<video_id>")
 def edit_video(video_id):
-    print(mongo.db.videos.find_one({"_id": video_id}))
     return render_template("edit_video.html",
                             video=mongo.db.videos.find_one({"_id": video_id}),
                             categories=mongo.db.categories.find(),
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from get_video_info import youtube_search, get_video_id_from_url
+from app import get_mongo_search_query, append_to_query
 
 
 class Test_Fitness_Vid(unittest.TestCase):
@@ -14,5 +15,12 @@ class Test_Fitness_Vid(unittest.TestCase):
         self.assertEqual(get_video_id_from_url('https://www.youtube.com/watch?v=DqGwxR_0d1M&feature=youtu.be'), 'DqGwxR_0d1M')
         self.assertEqual(get_video_id_from_url('https://www.youtube.com/watch?v=DqGwxR_0d1M'), 'DqGwxR_0d1M')
         
+    def test_get_mongo_search_query(self):
+        self.assertEqual(get_mongo_search_query('category_name', ["yoga"]), {'category_name': 'yoga'})
+        self.assertEqual(get_mongo_search_query('category_name', ["yoga", "cardio"]), {'$or': [{'category_name':'yoga'}, {'category_name':'cardio'}]})
+        self.assertEqual(get_mongo_search_query('category_name', ["yoga", "cardio", 'strenght']), {'$or': [{'category_name':'yoga'}, {'category_name':'cardio'}, {'category_name':'strenght'}]})
+    
 if __name__ == '__main__':
     unittest.main()
+    
+    
